b154403b1b7c3512b3a3af876e4b8335791720b7
projectPythonator <200~danjkstraProtonAlt1@protonmail.com>
2024-02-06 14:24:49 -0600

    added basic read functionallity to the backend module
    type annotations in as well
    TaskItem now a type
    
    functions added
    read_task_by_named_key
    read_all_tasks

--- SRC/to_do_app_backend.py
+++ SRC/to_do_app_backend.py
@@ -1,5 +1,6 @@
-from typing import Dict
+from typing import Dict, Tuple, List
 
+TaskItem = Tuple[str, str]
 ToDoTasks = Dict[str, str] | None  # for now, we shall keep this basic like this
 ListOfTasks = Dict[str, str]
 
@@ -16,3 +17,13 @@ def create_task(task_name: str, task_description: str) -> None:
     """Creates a new task to be done."""
     global to_do_tasks
     to_do_tasks[task_name] = task_description
+
+
+def read_task_by_named_key(task_key_name: str):
+    global to_do_tasks
+    return to_do_tasks[task_key_name]
+
+
+def read_all_tasks() -> List[TaskItem]:
+    global to_do_tasks
+    return [(task_name, task_description) for task_name, task_description in to_do_tasks.items()]

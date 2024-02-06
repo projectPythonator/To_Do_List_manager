952cf4e97d0f206f6a29f3bc74a7f8869e185384
projectPythonator <200~danjkstraProtonAlt1@protonmail.com>
2024-02-06 14:45:51 -0600

    reordered file backend so its consistent
    reason some functions had exceptions first some didn't
    should read a bit better

--- SRC/to_do_app_backend.py
+++ SRC/to_do_app_backend.py
@@ -18,21 +18,21 @@ def create_tasks(new_tasks: ListOfTasks) -> None:
 def create_task_by_name_and_description(task_name: str, task_description: str) -> None:
     """Creates a new task to be done."""
     global to_do_tasks
-    if task_name in to_do_tasks:
+    if task_name not in to_do_tasks:
+        to_do_tasks[task_name] = task_description
+    else:
         raise mvc_exc.TaskByNameAlreadyExistsOnCreation(
             "task {}, with description {} already exists".format(task_name, task_description))
-    else:
-        to_do_tasks[task_name] = task_description
 
 
 def read_task_by_named_key(task_key_name: str):
     """Grabs task by keyed name."""
     global to_do_tasks
-    if task_key_name not in to_do_tasks:
+    if task_key_name in to_do_tasks:
+        return to_do_tasks[task_key_name]
+    else:
         raise mvc_exc.TaskNameOnReadDoesNotExist(
             "Task with name {} was not found in list of tasks".format(task_key_name))
-    else:
-        return to_do_tasks[task_key_name]
 
 
 def read_all_tasks() -> List[TaskItem]:
@@ -42,9 +42,11 @@ def read_all_tasks() -> List[TaskItem]:
 
 
 def update_task_given_keyed_name(task_key_name: str, new_description: str) -> None:
+    """Takes task key and new description and tries to update the task."""
     global to_do_tasks
     if task_key_name in to_do_tasks:
         to_do_tasks[task_key_name] = new_description
     else:
         raise mvc_exc.TaskNameOnUpdateDoesNotExist(
             "Task name {} and description {} was not found".format(task_key_name, new_description))
+

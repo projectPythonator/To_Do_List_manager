1c75a7a54b8773cbf3866b79fc2a9eb656004cfb
projectPythonator <200~danjkstraProtonAlt1@protonmail.com>
2024-02-07 15:25:43 -0600

    changed up the layout it looks a bit cleaner
    imporvements
    move exit button away from other buttons
    mmove the bottoms away from text boxes

--- SRC/model_view_controller.py
+++ SRC/model_view_controller.py
@@ -47,8 +47,7 @@ class Controller(object):
         user_name_prompt_string = 'enter user name here'
         task_name_prompt_string = 'enter task name here'
         task_description_prompt_string = 'enter task description here'
-        task_name_update_string = 'enter task name to update or delete here'
-        task_description_update_string = 'enter new task description here'
+        task_info_label_string = 'List of Tasks'
         gui = Tk()
         gui.configure(background="light green")
         gui.title("to do app")
@@ -71,14 +70,8 @@ class Controller(object):
         self.task_name_prompt_label = Label(gui, text=task_name_prompt_string,
                                             background="light green")
         self.task_name_text_field = Entry(gui)
+        self.text_info_label = Label(gui, text=task_info_label_string, background="light green")
         self.task_info_text_area = Text(gui, height=20, width=50)
-
-        self.task_num_prompt_label = Label(gui, text=task_name_update_string,
-                                           background="light green")
-        self.task_update_prompt_label = Label(gui, text=task_description_update_string,
-                                              background="light green")
-        self.task_num_text_field = Entry(gui)
-        self.task_update_text_field = Entry(gui)
 
         self.add_task_btn = Button(gui, text="Add task", fg="Black", bg="Red",
                                    command=self.add_task)
@@ -101,15 +94,10 @@ class Controller(object):
         self.task_info_text_field.grid(row=8, column=0)
         self.add_task_btn.grid(row=9, column=0)
 
-        self.task_num_prompt_label.grid(row=10, column=0)
-        self.task_num_text_field.grid(row=11, column=0)
-
-        self.task_update_prompt_label.grid(row=12, column=0)
-        self.task_update_text_field.grid(row=13, column=0)
-
-        self.update_task_btn.grid(row=14, column=0)
-        self.delete_task_btn.grid(row=15, column=0)
-        self.task_info_text_area.grid(row=0, column=1, rowspan=14)
+        self.update_task_btn.grid(row=10, column=0)
+        self.delete_task_btn.grid(row=11, column=0)
+        self.text_info_label.grid(row=0, column=1)
+        self.task_info_text_area.grid(row=1, column=1, rowspan=12)
         self.exit_btn.grid(row=16, column=0)
         self.view.update_tasks_window(self.task_info_text_area, self.model.read_tasks())
         gui.mainloop()
@@ -152,8 +140,8 @@ class Controller(object):
         self.task_name_text_field.delete(0, END)
 
     def update_task(self):
-        task_description: str = self.task_update_text_field.get()
-        task_name = self.task_num_text_field.get()
+        task_description: str = self.task_info_text_field.get()
+        task_name = self.task_name_text_field.get()
         if len(task_description) == 0 or len(task_name) == 0:
             messagebox.showerror("empty text box",
                                  message="both task name and description must not be blank")
@@ -162,18 +150,18 @@ class Controller(object):
         self.model.create_task(task_name, task_description)
         self.view.update_tasks_window(self.task_info_text_area,
                                       self.model.read_tasks())
-        self.task_update_text_field.delete(0, END)
-        self.task_num_text_field.delete(0, END)
+        self.task_name_text_field.delete(0, END)
+        self.task_info_text_field.delete(0, END)
 
     def delete_task(self):
-        content: str = self.task_num_text_field.get()
+        content: str = self.task_name_text_field.get()
         if len(content) == 0:
             messagebox.showerror("task name error", message="task name must not be blank")
             return
         if len(self.model.read_tasks()) == 0:
             messagebox.showerror("No task", message="task list was empty")
             return
-        self.task_num_text_field.delete(0, END)
+        self.task_name_text_field.delete(0, END)
         self.model.delete_task(content)
         self.view.update_tasks_window(self.task_info_text_area,
                                       self.model.read_tasks())

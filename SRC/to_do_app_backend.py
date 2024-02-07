3890a332b41593426899689c56dd521a38a4c5d9
projectPythonator <200~danjkstraProtonAlt1@protonmail.com>
2024-02-07 09:26:01 -0600

    finsied up the db stuff for backend

--- SRC/to_do_app_backend.py
+++ SRC/to_do_app_backend.py
@@ -120,85 +120,33 @@ def read_tasks(conn, user_name):
     return result
 
 
-
-
-
-
-def create_tasks(new_tasks: ListOfTasks) -> None:
-    """Sets the current list of task to a new list of tasks."""
-    global to_do_tasks
-    to_do_tasks = new_tasks
-
-
-def create_task_by_name_and_description(task_name: str, task_description: str) -> None:
-    """Creates a new task to be done."""
-    global to_do_tasks
-    if task_name not in to_do_tasks:
-        to_do_tasks[task_name] = task_description
+@connect
+def update_task(conn, task_name, task_description, user_name):
+    user_name = scrub(user_name)
+    sql_check_command = 'SELECT EXISTS(SELECT 1 FROM {} WHERE name=? LIMIT 1'.format(user_name)
+    sql_update_command = 'UPDATE {} SET description=? WHERE name=?'.format(user_name)
+    connect_obj = conn.execute(sql_check_command, (task_name,))
+    result = connect_obj.fetchone()
+    if result[0]:
+        connect_obj.execute(sql_update_command, (task_description, task_name))
+        conn.commit()
     else:
-        raise mvc_exc.TaskNameOnCreationAlreadyExists(
-            "task {}, with description {} already exists".format(task_name, task_description))
+        mvc_exc.TaskNameOnUpdateDoesNotExist(
+            "cant update '{}' because it does not exist for user {}".format(task_name, user_name))
 
 
-def read_task_by_named_key(task_key_name: str):
-    """Grabs task by keyed name."""
-    global to_do_tasks
-    if task_key_name in to_do_tasks:
-        return to_do_tasks[task_key_name]
+@connect
+def delete_task(conn, task_name, user_name):
+    user_name = scrub(user_name)
+    sql_check_command = 'SELECT EXISTS(SELECT 1 FROM {} WHERE name=? LIMIT 1'.format(user_name)
+    user_name = scrub(user_name)
+    sql_delete_command = 'DELETE FROM {} WHERE name=?'.format(user_name)
+    connect_obj = conn.execute(sql_check_command, (task_name,))
+    result = connect_obj.fetchone()
+    if result[0]:
+        connect_obj.execute(sql_delete_command, (task_name,))
+        conn.commit()
     else:
-        raise mvc_exc.TaskNameOnReadDoesNotExist(
-            "Task with name {} was not found in list of tasks".format(task_key_name))
+        mvc_exc.UserNameOnDeleteDoesNotExist(
+            "cant delete '{}' because it does not exist for user {}".format(task_name, user_name))
 
-
-def read_all_tasks() -> List[TaskItem]:
-    """Grabs all tasks that currently exist."""
-    global to_do_tasks
-    return [(task_name, task_description) for task_name, task_description in to_do_tasks.items()]
-
-
-def update_task_given_keyed_name(task_key_name: str, new_description: str) -> None:
-    """Takes task key and new description and tries to update the task."""
-    global to_do_tasks
-    if task_key_name in to_do_tasks:
-        to_do_tasks[task_key_name] = new_description
-    else:
-        raise mvc_exc.TaskNameOnUpdateDoesNotExist(
-            "Task name {} and description {} was not found".format(task_key_name, new_description))
-
-
-def delete_task_given_keyed_name(task_key_name: str) -> None:
-    """Delete given task if it exists"""
-    global to_do_tasks
-    if task_key_name in to_do_tasks:
-        to_do_tasks.pop(task_key_name)
-    else:
-        raise mvc_exc.TaskNameOnDeleteDoesNotExist(
-            "Task name {} not found when deletion attempted".format(task_key_name))
-
-
-def create_user_by_user_name(user_name: str) -> None:
-    global users
-    if user_name not in users:
-        users[user_name] = {}
-    else:
-        raise mvc_exc.UserNameOnCreationAlreadyExists(
-            "User name {} already exists during creation".format(user_name))
-
-
-def delete_user_by_user_name(user_name: str) -> None:
-    global users
-    if user_name in users:
-        del users[user_name]
-    else:
-        raise mvc_exc.UserNameOnDeleteDoesNotExist(
-            "User name {} does not exist during deletion".format(user_name))
-
-
-def load_user_by_user_name(user_name: str) -> None:
-    global users, current_user
-    if user_name in users:
-        current_user = user_name
-        create_tasks(users[user_name])
-    else:
-        raise mvc_exc.UserNameOnDeleteDoesNotExist(
-            "User name {} does not exist during deletion".format(user_name))

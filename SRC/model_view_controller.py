d43633fdac42065bf907d7bf28ae214520c7d590
projectPythonator <200~danjkstraProtonAlt1@protonmail.com>
2024-02-06 19:29:39 -0600

    implemented the basic functions for the model class

--- SRC/model_view_controller.py
+++ SRC/model_view_controller.py
@@ -3,9 +3,27 @@ import mvc_exceptions as mvc_exc
 
 
 class Model(object):
-    def __init__(self):
+    def __init__(self, user_info):
+        backend_file.create_tasks(user_info)  # load up the info for a user
         pass
 
+    def create_task(self, task_key_name, task_description):
+        backend_file.create_task_by_name_and_description(task_key_name, task_description)
+
+    def create_tasks(self, list_of_tasks):
+        backend_file.create_tasks(list_of_tasks)
+
+    def read_task(self, task_key):
+        return backend_file.read_task_by_named_key(task_key)
+
+    def read_tasks(self):
+        return backend_file.read_all_tasks()
+
+    def update_task(self, task_key, task_description):
+        backend_file.update_task_given_keyed_name(task_key, task_description)
+
+    def delete_task(self, task_key):
+        backend_file.delete_task_given_keyed_name(task_key)
 
 class View(object):
     def __init__(self):

a892319b9a81a362a48d848e5c4bdc9fa4af4a5e
projectPythonator <200~danjkstraProtonAlt1@protonmail.com>
2024-02-06 18:57:07 -0600

    added in classes to model_view_controller.py
    classes added
    Model
    View
    Controller

--- SRC/model_view_controller.py
+++ SRC/model_view_controller.py
@@ -2,3 +2,16 @@ import to_do_app_backend as backend_file
 import mvc_exceptions as mvc_exc
 
 
+class Model(object):
+    def __init__(self):
+        pass
+
+
+class View(object):
+    def __init__(self):
+        pass
+
+
+class Controller(object):
+    def __init__(self):
+        pass
